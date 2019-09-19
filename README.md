15dfd27afa7c44f207a08b3871de01ad126f9ec7
Winston Tsia <winstontsia@gmail.com>
2019-09-18 22:44:20 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Ship Class
+-methods take in general ship1 (aggresor) and ship2 (receiver)
+-methods needed for ship: taking damage, dealing damage, charging laser, capping laser capacity, activating shield, deactivating shield after turn,  
+
+Laser Mechanics
+If laser is 0, cannot shoot. Laser clip cannot go above 3. 
+
+Shield Mechanics
+If shield is on, then shield takes incoming damage if enemy ship fires clip.
+Shield is unlimited use, with boolean state.
--- game.js
+++ game.js
@@ -1,46 +1,127 @@
-//Ship moveset variables
-let playerLaserStorage = 0;
-let playerShield = 20;
-let playerLaserDamage = 10;
+//GAME BEGIN
+document.getElementById("start").addEventListener("click", function() {
+    console.log("start button works!");
+})
 
-//Class declaration
+//MUSIC & SOUNDS
+backgroundMusic = document.getElementById("backgroundMusic");
+backgroundMusic.volume = 0.2;
+
+function testfunction() {
+    console.log(`this works!`);
+}
+
+document.getElementById("fire").addEventListener("click", function() {
+    console.log(`pew pew`)
+    let laserSound = new Audio("pew.wav");
+    laserSound.play();
+});
+
+//ROUND COUNTER
+let totalRounds = 0;
+function addRound() {
+    totalRounds++;
+}
+
+//SHIP CLASS DECLARATION
 class Ship {
-    constructor(health, laserStorage, laserDamage, shield) {
+    constructor(health, laserClip = 0, laserDamage, shield = false) {
         this.health = health;
-        this.laserStorage = laserStorage;
+        this.laserClip = laserClip;
         this.laserDamage = laserDamage;
         this.shield = shield;
         }
+}
 
-    attack(ship1, ship2) {
-        ship1.health = ship1.health - ship2.damage;
+//ENEMY CLASS
+
+//Enemy Attacker
+class EnemyAttacker extends Ship {
+    moveChoice() {
+        if (playerShip.laserClip == 0) {
+            this.chargeLaser()
+        }
+        if (playerShip ) {
+
+        }
     }
-    chargeLaser() {
+}
 
+//Enemy Defender
+class EnemyDefender extends Ship {
+    moveChoice() {
+        if ()
     }
-    useShield() {
+}
 
+//PLAYER DECLARATION
+let playerShip = new Ship (100, 0, 20, false)
+console.log(playerShip);
+let enemyShip1 = new EnemyAttacker(100, 0, 10, false);
+
+//DIFFICULTY LEVELS
+// let levelChoice = {};
+
+// function startLevel() {
+//     let enemyShip1 = new EnemyAttacker(100, 0, 10, false);
+// }
+// function startLevel2() {
+//     let enemyShip1 = new EnemyAttacker(100, 0, 10, false);
+// }
+// function startLevel3() {
+//     let enemyShip1 = new EnemyAttacker(100, 0, 10, false);
+// }
+
+//FUNCTIONS
+function battle() {
+    attack();
+}
+
+function attack(shipAttacker, shipDefender) {
+    if (shipDefender.shield == false) {
+            if (shipAttacker.laserClip = 0) {
+                console.log("Cannot fire weapon! Ammunition is not loaded!");
+            }
+            if (shipAttacker.laserClip >= 0 && shipAttacker.laserClip <= 3) {
+                console.log("Fire fire fire!")
+                if (shipDefender.shield == true) {
+                    shipDefender.health = shipDefender.health - shipAttacker.damage;
+                }
+            }
+        shipDefender.health = shipDefender.health - shipAttacker.damage;
+        console.log("Enemy is hit!")
+    } if (shipDefender.shield == true) {
+        console.log("Enemy shield has deflected our shot!")
     }
 }
 
-class playerShip extends Ship {
-    
+function chargeLaser(ship1) {
+    ship1.laserClip++;
+    console.log(ship1.laserClip);
 }
 
-class Enemy extends Ship {
-
+function activateShield(ship1) {
+    console.log(ship1.shield);
+    ship1.shield = true;
 }
 
-//Game Loop
-
-//User Input
-document.getElementById('fire')
-document.getElementById('charge')
-document.getElementById('shield')
-
-function updateDisplayShip {
+function roundReset() {
 
 }
 
+//USER INPUT
+document.getElementById('fire').addEventListener("click", attack(playerShip, enemyShip1));
+document.getElementById('charge').addEventListener("click", chargeLaser(playerShip));
+document.getElementById('shield').addEventListener("click", activateShield(playerShip));
 
-function: 
+
+//GAME END STAGE CONDITIONS
+
+function gameEnd() {
+
+}
+
+//BONUS
+//Make a function that prints text slowly instead of instantly
+
+// function: 
